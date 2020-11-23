da9a88784dabb8f0af20a79de3aa178e81beb6c8
viktor <viktorkarlsson125@gmail.com>
2020-11-23 21:16:36 +0100

    changing test to only test when user pick rock bc if one work they all should work. cypress can't find button

--- cypress/integration/UserWinOrLose.feature.js
+++ cypress/integration/UserWinOrLose.feature.js
@@ -1,16 +1,21 @@
 describe("user win or lose", () => {
     beforeEach(() => {
-        cy.get('button').contains('Choose Rock').click()
+        cy.get('#button').contains('Choose Rock').click()
         cy.get("#playerSelection").should("contain", "You have chosen Rock")
     })
     
     it("user picks rock", () => {
-        cy.get("#playerSelection").then(("#computerCharacter") => {
-            if ("#computerCharacter"(3))) {
-                should("contain", "You Win")
-            } else {
-                should("contain", "You lost or tied")
-            }
-        })
+        cy.get("#ComputerCharacter").contains(1)
+        cy.get("#scoreMessage").should("contain", "it's a tie")
+    })
+
+    it("user picks paper", () => {
+        cy.get("#ComputerCharacter").contains(2)
+        cy.get("#scoreMessage").should("contain", "You lose")
+    })
+
+    it("user picks scissor", () => {
+        cy.get("#ComputerCharacter").contains(3)
+        cy.get("#scoreMessage").should("contain", "You win!")
     })
 })
--- src/computerCharacter.js
+++ src/computerCharacter.js
@@ -1,10 +1,10 @@
 class ComputerCharacter {
 
-    function computerCharacter(3) {
-        return Math.floor(Math.random() * Math.floor(3))
-    };
+    // function computerCharacter() {
+    //     return Math.floor(Math.random() * Math.floor(3))
+    // };
 
-    console.log(computerChoose(3))
+    // console.log(computerChoose(3))
 
 }
 
