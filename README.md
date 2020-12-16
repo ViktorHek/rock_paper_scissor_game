cde28590a5917e4c7f1e837953d3064de31e3dbb
viktor <viktorkarlsson125@gmail.com>
2020-12-16 19:20:09 +0100

    adding semantic-ui

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 This is a Rock, Paper, Scissors game as part of a challenge from the Craft Academy bootcamp. The challenge was simply to create an Rock, paper, Scissor game with no additional instructions. The ide was that we, the student, would have to think for our self and be creative when finding a solution to the challenge. My creativity stepped in when I choose to replace the traditional rock, paper and scissor to a fire pokemon, water pokemon and a grass pokemon. 
 
 The game is simple. You pick a pokemon based on it's element. If an element is super effective against another, then the pokemon with that element wins. witch means fire wins against grass, grass wins against water and water wins against fire. If you and your opponent chooses the same pokemon the game will tell you that "Pokemons don't fight there friends", meaning it's a tie. Both you and your opponent starts with the pokemon Ditto. Ditto only have one ability, to transform in to another pokemon. you choose if you want your Ditto to transform in to a fire, water ore grass pokemon and then you press the button that says Figth! Your opponents pokemon will be a random pokemon between the three options. it will also go through 5 options in 1 second before making it's pick. depending on your and the computers choice it will give you one out of three messages:
+
 * "You are the Champion" - if you win
 * "Pokemons don't fight there friends" - if you tie
 * "You lose!" - if you lose
@@ -15,7 +16,7 @@ In order to play a fun game
 I would like to play a game of Rock, Paper, Scissor 
 ```
 ```
-As a visitor
+As a developer
 In order to make it a unique game 
 I would like pokemons to replace the traditions rock, paper and scissor
 ```
--- src/game.js
+++ src/game.js
@@ -1,89 +1,107 @@
-import React, { Component } from 'react'
-import DisplayPlayer from "./DisplayPlayer"
-import "./App.css"
+import React, { Component } from "react";
+import DisplayPlayer from "./DisplayPlayer";
+import "./App.css";
+import { Button, Icon } from "semantic-ui-react";
 
-const pokemons = ["fire", "water", "grass", "starter"]
+const pokemons = ["fire", "water", "grass", "starter"];
 class Game extends Component {
-    
   state = {
     player: pokemons[3],
     computer: pokemons[3],
-    winner: ""
+    winner: "",
   };
 
   startFight = () => {
-  let count = 0;
+    let count = 0;
     let interval = setInterval(() => {
       count++;
       this.setState({
         computer: pokemons[Math.floor(window.Math.random() * 3)],
-        winner: ""
-      })
-      if(count > 5) {
+        winner: "",
+      });
+      if (count > 5) {
         clearInterval(interval);
         this.setState({
-          winner: this.champion()
-        })
+          winner: this.champion(),
+        });
       }
-    }, 100)
-  }
+    }, 100);
+  };
 
   champion = () => {
-  const { player, computer } = this.state
-    if ((player === "fire" && computer === "grass") ||
+    const { player, computer } = this.state;
+    if (
+      (player === "fire" && computer === "grass") ||
       (player === "grass" && computer === "water") ||
-      (player === "water" && computer === "fire")) {
-      return "You are the Champion"
+      (player === "water" && computer === "fire")
+    ) {
+      return "You are the Champion";
     } else if (player === computer) {
-      return "Pokemons don't fight there friends"
+      return "Pokemons don't fight there friends";
     } else {
-      return "You lose!"
+      return "You lose!";
     }
-  }
+  };
 
   choosePokemon = (pokemon) => {
     this.setState({
       player: pokemon,
-    })
-  }
+    });
+  };
 
   render() {
-  const { player, computer, winner } = this.state
+    const { player, computer, winner } = this.state;
     return (
       <div className="hero-container">
-      <video src="./video.mp4" autoPlay loop muted />
+        <video src="./video.mp4" autoPlay loop muted />
         <h1>Rock Paper Scissor. pokemon edition!</h1>
         <div id="playerSelection">
           <DisplayPlayer pokemon={player} />
           <DisplayPlayer pokemon={computer} />
         </div>
         <div class="selection">
-          <button 
-            className="selectPokemon" 
-            data-cy="fire" 
+          <Button
+            animated
+            data-cy="fire"
             onClick={() => this.choosePokemon("fire")}
-          >Choose Fire
-          </button>
-          <button 
-            className="selectPokemon" 
-            data-cy="water" 
+          >
+            <Button.Content visible>Choose Fire</Button.Content>
+            <Button.Content hidden>
+              <Icon name="fire" />
+            </Button.Content>
+          </Button>
+          <Button
+            animated
+            data-cy="water"
             onClick={() => this.choosePokemon("water")}
-          >Choose Water
-          </button>
-          <button 
-            className="selectPokemon" 
-            data-cy="grass" 
+          >
+            <Button.Content visible>Choose Water</Button.Content>
+            <Button.Content hidden>
+              <Icon name="tint" />
+            </Button.Content>
+          </Button>
+          <Button
+            animated
+            data-cy="grass"
             onClick={() => this.choosePokemon("grass")}
-          >Choose Grass
-          </button>
+          >
+            <Button.Content visible>Choose Grass</Button.Content>
+            <Button.Content hidden>
+              <Icon name="leaf" />
+            </Button.Content>
+          </Button>
         </div>
 
-        <div className="winner" data-cy="winner">{winner && this.champion()}</div>
+        <div className="winner" data-cy="winner">
+          {winner && this.champion()}
+        </div>
 
-        <button type="button" data-cy="startFight" onClick={this.startFight}>Fight!</button>
+        <Button type="button" data-cy="startFight" onClick={this.startFight}>
+          Fight!
+        </Button>
       </div>
-    )
+    );
   }
 }
 
-export default Game
+export default Game;
