94daf4ad51e4cc6371c42c5076a7d335b1247bbb
viktor <viktorkarlsson125@gmail.com>
2020-12-16 17:50:12 +0100

    functionality and test done. but need styling and README update

--- cypress/integration/UserWinOrLose.feature.js
+++ cypress/integration/UserWinOrLose.feature.js
@@ -1,27 +1,115 @@
 describe("user win or lose", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:3000", {
-            OnBeforeLoad(window) {
-                cy.stub(window.Math, "random").returns(1)
-            }
-        })
-    })
-    
-    it("user picks fire", () => {
-        cy.get("[data-cy='fire']").contains('Choose Fire').click()
-        cy.get("[data-cy='startFight']").click()
-        cy.get("[data-cy='winner']").should("contain", "You lose!")
-    })
+  describe("computer choose fire", () => {
+    it("user picks fire and they tie", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(0);
+        },
+      });
+      cy.get("[data-cy='fire']").contains("Choose Fire").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should(
+        "contain",
+        "Pokemons don't fight there friends"
+      );
+    });
 
-    it("user picks water", () => {
-        cy.get("[data-cy='water']").contains("Choose Water").click()
-        cy.get("[data-cy='startFight']").click()
-        cy.get("[data-cy='winner']").should("contain", "Pokemons don't fight there friends")
-    })
+    it("user picks water and wins", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(0);
+        },
+      });
+      cy.get("[data-cy='water']").contains("Choose Water").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should("contain", "You are the Champion");
+    });
 
-    it("user picks grass", () => {
-        cy.get("[data-cy='grass']").contains("Choose Grass").click()
-        cy.get("[data-cy='startFight']").click()
-        cy.get("[data-cy='winner']").should("contain", "You are the Champion")
-    })
-})
+    it("user picks grass and lose", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(0);
+        },
+      });
+      cy.get("[data-cy='grass']").contains("Choose Grass").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should("contain", "You lose!");
+    });
+  });
+
+  describe("computer choose water", () => {
+    it("user picks fire and lose", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(1);
+        },
+      });
+      cy.get("[data-cy='fire']").contains("Choose Fire").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should("contain", "You lose!");
+    });
+
+    it("user picks water and they tie", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(1);
+        },
+      });
+      cy.get("[data-cy='water']").contains("Choose Water").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should(
+        "contain",
+        "Pokemons don't fight there friends"
+      );
+    });
+
+    it("user picks grass and wins", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(1);
+        },
+      });
+      cy.get("[data-cy='grass']").contains("Choose Grass").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should("contain", "You are the Champion");
+    });
+  });
+
+  describe("computer choose grass", () => {
+    it("user picks fire and wins", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(2);
+        },
+      });
+      cy.get("[data-cy='fire']").contains("Choose Fire").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should("contain", "You are the Champion");
+    });
+
+    it("user picks water and lose", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(2);
+        },
+      });
+      cy.get("[data-cy='water']").contains("Choose Water").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should("contain", "You lose!");
+    });
+
+    it("user picks grass and they tie", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          cy.stub(window.Math, "floor").returns(2);
+        },
+      });
+      cy.get("[data-cy='grass']").contains("Choose Grass").click();
+      cy.get("[data-cy='startFight']").click();
+      cy.get("[data-cy='winner']").should(
+        "contain",
+        "Pokemons don't fight there friends"
+      );
+    });
+  });
+});
