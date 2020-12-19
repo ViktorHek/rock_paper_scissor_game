11003e3f24038401cf23298067c976536fd6d254
viktor <viktorkarlsson125@gmail.com>
2020-12-19 13:54:50 +0100

    refactoring cypress test to be more DRY with beforeEach

--- cypress/integration/UserWinOrLose.feature.js
+++ cypress/integration/UserWinOrLose.feature.js
@@ -1,11 +1,13 @@
 describe("user win or lose", () => {
   describe("computer choose fire", () => {
-    it("user picks fire and they tie", () => {
+    beforeEach(() => {
       cy.visit("/", {
         onBeforeLoad(window) {
           cy.stub(window.Math, "floor").returns(0);
         },
       });
+    });
+    it("user picks fire and they tie", () => {
       cy.get("[data-cy='fire']").contains("Choose Fire").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should(
@@ -15,22 +17,12 @@ describe("user win or lose", () => {
     });
 
     it("user picks water and wins", () => {
-      cy.visit("/", {
-        onBeforeLoad(window) {
-          cy.stub(window.Math, "floor").returns(0);
-        },
-      });
       cy.get("[data-cy='water']").contains("Choose Water").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should("contain", "You are the Champion");
     });
 
     it("user picks grass and lose", () => {
-      cy.visit("/", {
-        onBeforeLoad(window) {
-          cy.stub(window.Math, "floor").returns(0);
-        },
-      });
       cy.get("[data-cy='grass']").contains("Choose Grass").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should("contain", "You lose!");
@@ -38,23 +30,20 @@ describe("user win or lose", () => {
   });
 
   describe("computer choose water", () => {
-    it("user picks fire and lose", () => {
+    beforeEach(() => {
       cy.visit("/", {
         onBeforeLoad(window) {
           cy.stub(window.Math, "floor").returns(1);
         },
       });
+    });
+    it("user picks fire and lose", () => {
       cy.get("[data-cy='fire']").contains("Choose Fire").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should("contain", "You lose!");
     });
 
     it("user picks water and they tie", () => {
-      cy.visit("/", {
-        onBeforeLoad(window) {
-          cy.stub(window.Math, "floor").returns(1);
-        },
-      });
       cy.get("[data-cy='water']").contains("Choose Water").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should(
@@ -64,11 +53,6 @@ describe("user win or lose", () => {
     });
 
     it("user picks grass and wins", () => {
-      cy.visit("/", {
-        onBeforeLoad(window) {
-          cy.stub(window.Math, "floor").returns(1);
-        },
-      });
       cy.get("[data-cy='grass']").contains("Choose Grass").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should("contain", "You are the Champion");
@@ -76,34 +60,26 @@ describe("user win or lose", () => {
   });
 
   describe("computer choose grass", () => {
-    it("user picks fire and wins", () => {
+    beforeEach(() => {
       cy.visit("/", {
         onBeforeLoad(window) {
           cy.stub(window.Math, "floor").returns(2);
         },
       });
+    });
+    it("user picks fire and wins", () => {
       cy.get("[data-cy='fire']").contains("Choose Fire").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should("contain", "You are the Champion");
     });
 
     it("user picks water and lose", () => {
-      cy.visit("/", {
-        onBeforeLoad(window) {
-          cy.stub(window.Math, "floor").returns(2);
-        },
-      });
       cy.get("[data-cy='water']").contains("Choose Water").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should("contain", "You lose!");
     });
 
     it("user picks grass and they tie", () => {
-      cy.visit("/", {
-        onBeforeLoad(window) {
-          cy.stub(window.Math, "floor").returns(2);
-        },
-      });
       cy.get("[data-cy='grass']").contains("Choose Grass").click();
       cy.get("[data-cy='startFight']").click();
       cy.get("[data-cy='winner']").should(
